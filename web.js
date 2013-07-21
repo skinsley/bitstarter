ad531030edbc1f503692ba02c647e00a8c224ec7
Stephen Kinsley <stephen.kinsley@gmail.com>
2013-07-21 23:51:21 +0000

    corrected web.js

--- web.js
+++ web.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var buffer = new Buffer(fs.readFileSync(index.html))
+var fs = require('fs');
+
 
 app.get('/', function(request, response) {
-  response.send(buf.toString());
+  response.send(fs.readFileSync("index.html").toString());
 });
 
 var port = process.env.PORT || 5000;
